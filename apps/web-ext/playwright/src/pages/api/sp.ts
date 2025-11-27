bc282ade52ecac3ebc3f153765a5b0c57447e6ac
Kohei Watanabe <nebel@fogtype.com>
2025-11-27 15:39:06 +0900

    feat(web-ext): サンプルの再構築と更新 (#209)
    Signed-off-by: Kohei Watanabe <nebel@fogtype.com>

---
+++ apps/web-ext/playwright/src/pages/api/sp.ts
@@ -0,0 +1,28 @@
+import { sign, writeJson } from "./_utils";
+
+export async function POST(): Promise<Response> {
+  const {
+    0: core,
+    1: annotation,
+    2: media,
+    3: site,
+  } = await Promise.all([
+    sign("_core.json"),
+    sign("_annotation.json"),
+    sign("_media.json"),
+    sign("_site.json"),
+  ]);
+
+  await writeJson("../../../public/.well-known/sp.json", {
+    originators: [
+      {
+        core,
+        annotations: [annotation],
+        media,
+      },
+    ],
+    credential: site,
+  });
+
+  return new Response("ok");
+}
