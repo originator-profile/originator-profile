20f67eadf8a5adaca4df91f330e0fcb468a8dbd6
Kohei Watanabe <nebel@fogtype.com>
2025-11-27 14:55:18 +0900

    add astro.config.ts
    Signed-off-by: Kohei Watanabe <nebel@fogtype.com>

--- apps/web-ext/playwright.config.ts
+++ apps/web-ext/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   webServer: {
     url: "http://localhost:8080",
-    command: "astro --root=playwright dev --port=8080",
+    command: "astro --root=playwright dev",
     reuseExistingServer: !process.env.CI,
   },
   globalSetup: "e2e/global-setup.ts",
