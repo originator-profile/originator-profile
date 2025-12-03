6fd95db3460515a2cc6da0e89a07eba6fb1ed304
Kimiaki Kuno <knokmki612@gmail.com>
2025-12-03 14:24:17 +0900

    refactor(webext-core): compatSendMessage 廃止
    公式で frameId 指定可能になったため
    
    see https://github.com/aklinker1/webext-core/releases/tag/messaging-v2.2.0
    
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- apps/web-ext/src/components/credentials/events.ts
+++ apps/web-ext/src/components/credentials/events.ts
@@ -1,32 +1,6 @@
-import {
-  defineExtensionMessaging,
-  GetDataType,
-  GetReturnType,
-} from "@webext-core/messaging";
+import { defineExtensionMessaging } from "@webext-core/messaging";
 import { FetchCredentialsMessageResponse } from "./types";
 import { VerifyIntegrity } from "@originator-profile/verify";
-
-/** frameId 指定可能な defineExtensionMessaging() */
-export function compatDefineExtensionMessaging<
-  TProtocolMap extends Record<string, unknown>,
->() {
-  const messenger = defineExtensionMessaging<TProtocolMap>();
-  async function compatSendMessage<TType extends keyof TProtocolMap>(
-    type: TType,
-    data: GetDataType<TProtocolMap[TType]>,
-    tabId: number,
-    frameId: number,
-  ): Promise<GetReturnType<TProtocolMap[TType]>> {
-    const message = { type, data, timestamp: Date.now() };
-    const options = { frameId };
-    const { res } = await chrome.tabs.sendMessage(tabId, message, options);
-    return res;
-  }
-  return {
-    ...messenger,
-    compatSendMessage,
-  };
-}
 
 type CredentialsProtocolMap = {
   fetchCredentials(message: null): FetchCredentialsMessageResponse;
@@ -36,4 +10,4 @@ type CredentialsProtocolMap = {
 };
 
 export const credentialsMessenger =
-  compatDefineExtensionMessaging<CredentialsProtocolMap>();
+  defineExtensionMessaging<CredentialsProtocolMap>();
