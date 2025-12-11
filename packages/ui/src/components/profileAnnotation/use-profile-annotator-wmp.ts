0a9a73e89e8b6809d566f9ceccbaba7b6ce4c8a0
Kimiaki Kuno <knokmki612@gmail.com>
2025-12-11 18:31:05 +0900

    memorize in useProfileAnnotatorWmp hook Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/ui/src/components/profileAnnotation/use-profile-annotator-wmp.ts
+++ packages/ui/src/components/profileAnnotation/use-profile-annotator-wmp.ts
@@ -1,12 +1,17 @@
-import type { VerifiedOps } from "@originator-profile/verify";
 import type { WebMediaProfile } from "@originator-profile/model";
+import type { VerifiedOps } from "@originator-profile/verify";
+import { useMemo } from "react";
 
 export function useProfileAnnotatorWmp(
   ops: VerifiedOps,
   profileAnnotatorOpId: string,
 ): WebMediaProfile | undefined {
-  const profileAnnotatorOp = ops.find(
-    (op) => op.media?.doc.credentialSubject.id === profileAnnotatorOpId,
+  const profileAnnotatorOp = useMemo(
+    () =>
+      ops.find(
+        (op) => op.media?.doc.credentialSubject.id === profileAnnotatorOpId,
+      ),
+    [ops, profileAnnotatorOpId],
   );
   return profileAnnotatorOp?.media?.doc;
 }
