f107cb6f46384157c50dc14d563fc48ef5e023bb
Kimiaki Kuno <knokmki612@gmail.com>
2025-12-11 15:23:34 +0900

    refactor: mt-0 は要素を種別で限定する必要なし
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- apps/web-ext/src/templates/Org.tsx
+++ apps/web-ext/src/templates/Org.tsx
@@ -54,7 +54,7 @@ function ReliabilityInfo(props: {
         <Description
           description={props.wmp.credentialSubject.description}
           onlyBody={true}
-          className="text-black [&_:where(p,ul,ol,li)]:my-2 [&_:where(p,ul,ol,li):first-child]:mt-0"
+          className="text-black [&_:where(p,ul,ol,li)]:my-2 [&_:first-child]:mt-0"
         />
       )}
       {props.wmp.credentialSubject.informationTransmissionPolicy && (
