943ed07d4a28b949d867ac27ffca6dbba522e38b
Kimiaki Kuno <knokmki612@gmail.com>
2025-12-11 17:01:07 +0900

    fix complecity (12 to <10)
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/ui/src/components/profileAnnotation/CertificateDetail.tsx
+++ packages/ui/src/components/profileAnnotation/CertificateDetail.tsx
@@ -1,11 +1,12 @@
 import { Icon } from "@iconify/react";
+import type { CertificationSystem } from "@originator-profile/model";
 import { VerifiedVc } from "@originator-profile/securing-mechanism";
 import { Certificate, VerifiedOps } from "@originator-profile/verify";
 import { twMerge } from "tailwind-merge";
+import placeholderLogoMainUrl from "../../assets/placeholder-logo-main.png";
 import { _ } from "../../utils/get-message";
 import Image from "../Image";
 import Spinner from "../Spinner";
-import placeholderLogoMainUrl from "../../assets/placeholder-logo-main.png";
 import { useProfileAnnotatorWmp } from "./use-profile-annotator-wmp";
 
 type Props = {
@@ -14,18 +15,39 @@ type Props = {
   ops?: VerifiedOps;
 };
 
-function CertificateDetailContent({ certificate, ops }: Props) {
+function CertificateDescription(props: { description?: string }) {
+  if (!props.description) return null;
+  return <p className="text-sm text-gray-600">{props.description}</p>;
+}
+
+function CertificateRef(props: Pick<CertificationSystem, "ref">) {
+  if (!props.ref) return null;
+  return (
+    <a
+      className="card border px-5 py-3 flex items-center justify-between gap-2.5 rounded-2xl"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={props.ref}
+    >
+      <span className="flex flex-col gap-1">
+        <span className="text-xs text-gray-500">
+          {_("CertificateDetail_Details")}
+        </span>
+        <span className="text-sm">{props.ref}</span>
+      </span>
+      <Icon className="text-sm text-gray-500" icon="fa6-solid:arrow-right" />
+    </a>
+  );
+}
+
+function CertificateDetailContent({
+  certificate,
+  ops,
+}: Props & Required<Pick<Props, "certificate">>) {
   const paWmp = useProfileAnnotatorWmp(
     ops ?? [],
     certificate?.doc.issuer ?? "",
   );
-  if (!certificate)
-    return (
-      <div className="flex flex-col justify-center items-center gap-4 pt-6 pb-4">
-        <Spinner />
-        <p>{_("CertificateDetail_Loading")}</p>
-      </div>
-    );
   return (
     <>
       <header className="flex items-center gap-3 mb-4">
@@ -52,38 +74,17 @@ function CertificateDetailContent({ certificate, ops }: Props) {
           </p>
         </div>
       </header>
-      {"description" in certificate.doc.credentialSubject && (
-        <p className="text-sm text-gray-600">
-          {certificate.doc.credentialSubject.description}
-        </p>
-      )}
-      {"description" in
-        certificate.doc.credentialSubject.certificationSystem && (
-        <p className="text-sm text-gray-600">
-          {certificate.doc.credentialSubject.certificationSystem.description}
-        </p>
-      )}
-      {certificate.doc.credentialSubject.certificationSystem.ref && (
-        <a
-          className="card border px-5 py-3 flex items-center justify-between gap-2.5 rounded-2xl"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={certificate.doc.credentialSubject.certificationSystem.ref}
-        >
-          <span className="flex flex-col gap-1">
-            <span className="text-xs text-gray-500">
-              {_("CertificateDetail_Details")}
-            </span>
-            <span className="text-sm">
-              {certificate.doc.credentialSubject.certificationSystem.ref}
-            </span>
-          </span>
-          <Icon
-            className="text-sm text-gray-500"
-            icon="fa6-solid:arrow-right"
-          />
-        </a>
-      )}
+      <CertificateDescription
+        description={certificate.doc.credentialSubject.description}
+      />
+      <CertificateDescription
+        description={
+          certificate.doc.credentialSubject.certificationSystem.description
+        }
+      />
+      <CertificateRef
+        ref={certificate.doc.credentialSubject.certificationSystem.ref}
+      />
     </>
   );
 }
@@ -91,7 +92,14 @@ function CertificateDetailContent({ certificate, ops }: Props) {
 export function CertificateDetail({ className, certificate, ops }: Props) {
   return (
     <div className={twMerge("jumpu-card p-5 rounded-2xl space-y-3", className)}>
-      <CertificateDetailContent certificate={certificate} ops={ops} />
+      {certificate ? (
+        <CertificateDetailContent certificate={certificate} ops={ops} />
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-4 pt-6 pb-4">
+          <Spinner />
+          <p>{_("CertificateDetail_Loading")}</p>
+        </div>
+      )}
     </div>
   );
 }
