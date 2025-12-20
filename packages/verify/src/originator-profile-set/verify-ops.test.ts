1d460bfcbad0bc65ac61195a13adcba0c682cc01
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2025-12-21 00:30:21 +0900

    test: テストで有効期限を生成するときの秒以下の単位の切り捨て処理を修正 Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
    Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>

--- packages/verify/src/originator-profile-set/verify-ops.test.ts
+++ packages/verify/src/originator-profile-set/verify-ops.test.ts
@@ -130,8 +130,10 @@ describe("OPSの検証", async () => {
     from.setDate(from.getDate() - 1);
     const until = new Date();
     until.setDate(until.getDate() + 1);
-    certificateWithExpiry.validFrom = from.toISOString().replace(".000", "");
-    certificateWithExpiry.validUntil = until.toISOString().replace(".000", "");
+    certificateWithExpiry.validFrom = from.toISOString().replace(/\.\d{3}/, "");
+    certificateWithExpiry.validUntil = until
+      .toISOString()
+      .replace(/\.\d{3}/, "");
 
     const originatorOp = {
       core: await signCp(cp, authority.privateKey, signOptions),
@@ -196,7 +198,7 @@ describe("OPSの検証", async () => {
     const certificateWithExpiry: Certificate = structuredClone(certificate);
     const from = new Date();
     from.setDate(from.getDate() + 2);
-    certificateWithExpiry.validFrom = from.toISOString().replace(".000", "");
+    certificateWithExpiry.validFrom = from.toISOString().replace(/\.\d{3}/, "");
 
     const originatorOp = {
       core: await signCp(cp, authority.privateKey, signOptions),
@@ -224,7 +226,9 @@ describe("OPSの検証", async () => {
     const certificateWithExpiry: Certificate = structuredClone(certificate);
     const until = new Date();
     until.setDate(until.getDate() - 2);
-    certificateWithExpiry.validUntil = until.toISOString().replace(".000", "");
+    certificateWithExpiry.validUntil = until
+      .toISOString()
+      .replace(/\.\d{3}/, "");
 
     const originatorOp = {
       core: await signCp(cp, authority.privateKey, signOptions),
