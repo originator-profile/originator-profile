d2d9200beb2df9bb8800bf9c743aae2d0fb4390c
Kimiaki Kuno <knokmki612@gmail.com>
2026-01-19 18:01:24 +0900

    demo-page: `@cloudflare/vite-plugin` を使った実装に移行
    これにより
    
    - 開発環境と本番環境での振る舞いの一貫性が向上します
    - SP の開発環境での動作確認が可能になります（要 SP 再発行）
    
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/demo-page/vite.config.js
+++ packages/demo-page/vite.config.js
@@ -1,19 +1,25 @@
 // vite.config.ts
+import { cloudflare } from "@cloudflare/vite-plugin";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-  build: {
-    rollupOptions: {
-      input: {
-        "ja/index": path.resolve(__dirname, "ja/index.html"),
-        "en/index": path.resolve(__dirname, "en/index.html"),
-      },
-      output: {
-        // assets/images の中に元のファイル名で出力
-        assetFileNames: "assets/images/[name].[ext]",
+  environments: {
+    client:{
+      build: {
+        rollupOptions: {
+          input: {
+            "ja/index": path.resolve(__dirname, "ja/index.html"),
+            "en/index": path.resolve(__dirname, "en/index.html"),
+          },
+          output: {
+            // assets/images の中に元のファイル名で出力
+            assetFileNames: "assets/images/[name].[ext]",
+          },
+        },
       },
     },
   },
+  plugins: [cloudflare()],
 });
