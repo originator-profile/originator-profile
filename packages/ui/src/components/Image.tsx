a8714ca37ea5b3393059267a0ad9d9f08b481364
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2026-01-24 12:09:29 +0900

    add: 画像読み込み失敗した時の対処方をログに出力 Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>

--- packages/ui/src/components/Image.tsx
+++ packages/ui/src/components/Image.tsx
@@ -37,6 +37,15 @@ function Image({
         width={width}
         height={height}
         crossOrigin="anonymous"
+        onError={(e) => {
+          const locale = navigator.language;
+          const ja = locale.startsWith("ja");
+          const documentLink = `https://docs.originator-profile.org/${ja ? "ja" : "en"}/troubleshooting/image-access-error/`;
+          const message = ja
+            ? `[Originator Profile] 画像の読み込みに失敗しました: ${e.currentTarget.src}\n対処法: ${documentLink}`
+            : `[Originator Profile] Failed to load image: ${e.currentTarget.src}\nTroubleshooting: ${documentLink}`;
+          console.error(message);
+        }}
       />
     </figure>
   );
