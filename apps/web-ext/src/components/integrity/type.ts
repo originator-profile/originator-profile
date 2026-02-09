ebeeb064785887c49c1aad7341bc0d6909957999
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2026-02-09 23:16:37 +0900

    fix: content-scriptで発生したエラー型名を修正 Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>

--- apps/web-ext/src/components/integrity/type.ts
+++ apps/web-ext/src/components/integrity/type.ts
@@ -1,8 +1,8 @@
 import { IntegrityVerifyResult } from "@originator-profile/verify";
 
-export type FetchIntegrityFailed = string;
+export type FetchIntegrityFailedMessage = string;
 
 // FetchIntegrityResultをserializeIfErrorに通した時の表現
 export type FetchIntegrityMessageResult =
   | IntegrityVerifyResult
-  | FetchIntegrityFailed;
+  | FetchIntegrityFailedMessage;
