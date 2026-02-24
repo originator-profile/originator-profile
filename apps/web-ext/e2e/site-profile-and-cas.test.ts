bb9c67848eb25cd9abab01a0bb5a039ba8686180
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2026-02-24 12:43:47 +0900

    test: e2eテストの追加 Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>

--- apps/web-ext/e2e/site-profile-and-cas.test.ts
+++ apps/web-ext/e2e/site-profile-and-cas.test.ts
@@ -36,3 +36,35 @@ test("SiteProfile/CASの検証に成功するが、htmlに記載されたOPSの�
 
   await expect(ext?.getByTestId("cas")).toBeVisible();
 });
+
+test("CASの検証に成功するが、SiteProfileのWMPの取得に失敗した時にMissingの表示がされているか", async ({
+  context,
+  page,
+  missingMediaSiteProfile,
+  validCas: validCas,
+  missingOps: _,
+  credentialsPage,
+}) => {
+  await missingMediaSiteProfile(
+    { privateKey, publicKey },
+    credentialsPage.issuer,
+  );
+  await validCas(
+    { privateKey },
+    credentialsPage.contents,
+    credentialsPage.issuer,
+  );
+
+  await page.goto(credentialsPage.endpoint);
+  const ext = await popup(context);
+  await expect(ext?.getByTestId("site-profile")).toBeVisible();
+
+  const wspMissing = ext?.getByTestId("web-media-profile-missing");
+  await expect(wspMissing).toHaveCount(2);
+  expect(await wspMissing?.nth(0).innerText()).toBe(
+    "このサイト運営者に対応する組織情報を正しく読み取れませんでした",
+  );
+  expect(await wspMissing?.nth(1).innerText()).toBe(
+    "このサイト運営者に対応する組織情報を正しく読み取れませんでした",
+  );
+});
