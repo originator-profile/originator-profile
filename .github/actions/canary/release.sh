e93e4fc06f5d55082d5c322907d640d7146ea953
Kohei Watanabe <nebel@fogtype.com>
2026-03-05 12:10:04 +0900

    Refactor release.sh to simplify note handling
    Removed redundant note section and cleaned up the release script.
    
    Signed-off-by: Kohei Watanabe <nebel@fogtype.com>

--- .github/actions/canary/release.sh
+++ .github/actions/canary/release.sh
@@ -18,6 +18,9 @@ cd -
 NOTE="> [!WARNING]
 > **こちらは試験目的のビルドです。通常サイトの検証には[通常版](https://github.com/originator-profile/originator-profile/releases/latest)をご利用ください。**
 
+> [!WARNING]
+> **This is a build for testing purposes. Please use the [regular version](https://github.com/originator-profile/originator-profile/releases/latest) to verify the regular site.**
+
 <details>
 
 ## Download
@@ -31,29 +34,6 @@ Download from the Assets below.
 - WordPress Plugin wordpress-ca-manager.zip
 
 </details>"
-
-NOTE+="
----
-
-<details>
-<summary>English</summary>
-
-> [!WARNING]
-> **This is a build for testing purposes. Please use the [regular version](https://github.com/originator-profile/originator-profile/releases/latest) to verify the regular site.**
-
-## Download
-
-Download from the Assets below.
-
-- Testing Extension (Chrome) _testing_profile_web_extension-chromium-canary.zip
-- Testing Extension (Firefox) _testing_profile_web_extension-firefox-desktop-canary.zip
-- Extension (Chrome) profile_web_extension-chromium-canary.zip
-- Extension (Firefox) profile_web_extension-firefox-desktop-canary.zip
-- WordPress Plugin wordpress-ca-manager.zip
-
-</details>
-"
-
 
 gh release delete canary --yes --cleanup-tag 2>/dev/null || :
 gh release create canary --prerelease --title "Canary Release" --notes "$NOTE" .github/dist/*
