d7d8780bfc8acf97f9d6e76f81b2089c48248286
Kimiaki Kuno <knokmki612@gmail.com>
2026-03-16 14:17:57 +0900

    refactor!: deprecated models, errors, types, assets, func 廃止
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,4 @@
-export * from "./account";
 export * from "./content-attestation";
 export * from "./date";
 export * from "./safe-stringify";
 export * from "./select-by-locale";
-export * from "./typefunc";
