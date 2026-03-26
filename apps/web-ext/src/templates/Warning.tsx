3168d7d6bbd7b6ac207eaeec60b27991a18aa2f5
t4110 <t.ito@search-light.co.jp>
2026-03-26 02:26:07 +0000

    fix(web-ext): 広告遷移先警告ページのボタンをPrimaryカラーに変更

--- apps/web-ext/src/templates/Warning.tsx
+++ apps/web-ext/src/templates/Warning.tsx
@@ -63,7 +63,7 @@ function Warning({
         <div className="space-y-3">
           <button
             onClick={onBack}
-            className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200"
+            className="w-full bg-primary-700 text-white py-2 px-4 rounded hover:bg-primary-800 transition duration-200"
           >
             {backButtonLabel}
           </button>
