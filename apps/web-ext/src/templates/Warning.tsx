d5eec9d4188053805b8cf05fa3a3b0fda864a4e3
t4110 <t.ito@search-light.co.jp>
2026-03-26 04:06:05 +0000

    fix(web-ext): 広告遷移先警告ページの警告アイコンをIconifyのものに変更

--- apps/web-ext/src/templates/Warning.tsx
+++ apps/web-ext/src/templates/Warning.tsx
@@ -1,25 +1,5 @@
+import { Icon } from "@iconify/react";
 import { _ } from "@originator-profile/ui";
-
-const WarningIcon = () => {
-  return (
-    <div className="mb-4 text-red-500">
-      <svg
-        className="w-16 h-16 mx-auto"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-        />
-      </svg>
-    </div>
-  );
-};
 
 type Props = {
   sourceOrg: string | undefined;
@@ -43,7 +23,10 @@ function Warning({
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full text-center">
-        <WarningIcon />
+        <Icon
+          className="mb-4 w-16 h-16 mx-auto text-red-500"
+          icon="fa6-solid:triangle-exclamation"
+        />
         <h1 className="text-xl font-bold text-gray-800 mb-2">
           {_("Warning_Title")}
         </h1>
