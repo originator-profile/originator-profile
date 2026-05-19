757fb8019f6d32ddc20e86ab9f2c41e1f0d3f977
Kimiaki Kuno <knokmki612@gmail.com>
2026-05-19 17:56:31 +0900

    refactor(pa model): ベースモデルから定義されたプロパティは継承する
    Signed-off-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/model/src/profile-annotation/japanese-existence-pa.ts
+++ packages/model/src/profile-annotation/japanese-existence-pa.ts
@@ -1,38 +1,24 @@
 import { z } from "zod";
 import { OpCipContext } from "../context/op-cip-context";
-import { DateTimeStamp } from "../date-time-stamp";
-import { Image } from "../image";
-import { OpId } from "../op-id";
-import { ProfileAnnotationPolicy } from "./profile-annotation-policy";
+import { ProfileAnnotation } from "./profile-annotation";
+import { ProfileAnnotationProperties } from "./profile-annotation-properties";
 
-export const JapaneseExistencePAProperties = z.object({
-  id: OpId.describe("OP ID of the subject"),
-  type: z.literal("JP-OrganizationExistenceCertificate"),
-  name: z.string().optional().describe("PA name"),
-  description: z.string().optional().describe("Description"),
-  image: Image.optional(),
-  corporateName: z.string().describe("Corporate name"),
-  corporateNumber: z.string().describe("Corporate number"),
-  postalCode: z.string().describe("Postal code"),
-  addressCountry: z.string().describe("Country"),
-  addressRegion: z.string().describe("Prefecture / State"),
-  addressLocality: z.string().describe("City / Municipality"),
-  streetAddress: z.string().describe("Street address"),
-  annotation: ProfileAnnotationPolicy,
-});
+export const JapaneseExistencePAProperties = ProfileAnnotationProperties.extend(
+  {
+    type: z.literal("JP-OrganizationExistenceCertificate"),
+    corporateName: z.string().describe("Corporate name"),
+    corporateNumber: z.string().describe("Corporate number"),
+    postalCode: z.string().describe("Postal code"),
+    addressCountry: z.string().describe("Country"),
+    addressRegion: z.string().describe("Prefecture / State"),
+    addressLocality: z.string().describe("City / Municipality"),
+    streetAddress: z.string().describe("Street address"),
+  },
+);
 
-export const JapaneseExistencePA = z.looseObject({
+export const JapaneseExistencePA = ProfileAnnotation.extend({
   "@context": OpCipContext,
-  type: z.tuple([
-    z.literal("VerifiableCredential"),
-    z.literal("ProfileAnnotation"),
-  ]),
-  issuer: OpId,
   credentialSubject: JapaneseExistencePAProperties,
-  validFrom: DateTimeStamp.optional().describe(
-    "Validity period start and time",
-  ),
-  validUntil: DateTimeStamp.optional().describe("Validity period end and time"),
 });
 
 export type JapaneseExistencePA = z.infer<typeof JapaneseExistencePA>;
