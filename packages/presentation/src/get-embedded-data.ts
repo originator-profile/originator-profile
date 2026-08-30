c8b004002884322381ce6dfcadd98ba273af592b
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2026-08-30 21:58:40 +0900

    fix: コードレビュー対応
    Signed-off-by: Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>

--- packages/presentation/src/get-embedded-data.ts
+++ packages/presentation/src/get-embedded-data.ts
@@ -10,25 +10,22 @@ export function getEmbeddedData<T extends unknown[]>(
   mediaType: string,
 ): SourcedCredential<T[number]>[] {
   const elements = [...doc.querySelectorAll(`script[type="${mediaType}"]`)];
-  const dataArray = elements
-    .map((elem, elementIndex) => {
-      const text = elem.textContent;
-      if (typeof text !== "string") {
-        return undefined;
-      }
-      let json: unknown;
-      try {
-        json = JSON.parse(text);
-      } catch (e: unknown) {
-        return undefined;
-      }
-      const items = (Array.isArray(json) ? json : [json]) as T;
-      return items.map((credential) => ({
-        credential: credential,
-        source: embeddedSource(elementIndex),
-      }));
-    })
-    .filter((e) => typeof e !== "undefined");
 
-  return dataArray.flat();
+  return elements.flatMap((elem, elementIndex) => {
+    const text = elem.textContent;
+    if (typeof text !== "string") {
+      return [];
+    }
+    let json: unknown;
+    try {
+      json = JSON.parse(text);
+    } catch (e: unknown) {
+      return [];
+    }
+    const items = (Array.isArray(json) ? json : [json]) as T;
+    return items.map((credential) => ({
+      credential: credential,
+      source: embeddedSource(elementIndex),
+    }));
+  });
 }
